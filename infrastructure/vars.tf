7bdebb049d3eba284cef32dcf1cefe99cf85d16c
James Groeneveld <jamesgroeneveld.work@gmail.com>
2018-12-02 12:27:03 -0800

    infra(aws): setup aws provider and initial vars

---
+++ infrastructure/vars.tf
@@ -0,0 +1,33 @@
+variable "AWS_KEY" {
+  default = "FAKE_AWS_KEY"
+}
+variable "AWS_SECRET" {
+  default = "FAKE_AWS_SECRET"
+}
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "kinesis" {
+  default = "http://localhost:4568"
+}
+
+variable "dynamodb" {
+  default = "http://localhost:4569"
+}
+
+variable "dynamodbstreams" {
+  default = "http://localhost:4570"
+}
+
+variable "lambda" {
+  default = "http://localhost:4574"
+}
+
+variable "sns" {
+  default = "http://localhost:4575"
+}
+
+variable "s3" {
+  default = "http://localhost:4572"
+}
